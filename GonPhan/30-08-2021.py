1ed3fa1d04f6dedc56e40dd053cc781839ecd40e
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-08-30 22:48:49 +0700

    Update

--- GonPhan/30-08-2021.py
+++ GonPhan/30-08-2021.py
@@ -1,6 +1,17 @@
-# Cau A:
-#     Imput: a = [1, 2, 3, 4, 5]
-#     Output: 5 4 3 2 1
-# Cau B:
-#     Input a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-#     OutPut [[1], [2, 3], [4, 5, 6], [7, 8, 9, 10]]
+from typing import type_check_only
+
+
+# Bài 1:
+
+# Đếm số chẵn trong mảng A 
+# Input: A = [1, 2, 3, 4, 4]
+# Output: 3
+# Vì có 3 số chẵn trong dãy
+
+# Bài 2:
+# Tìm số xuất hiện nhiều nhất trong dãy a. Nếu có nhiều số xuất hiện cùng số lần, đưa ra số lớn nhất.
+# a = [1, 2, 2, 3, 2, 5]
+# Kết quả là 2 vì: 2 xuất hiện 3 lần, là nhiều nhất
+# a = [1, 2, 3, 2, 3]
+# Kết quả là 3, vì 3 xuất hiện 2 lần, số 2 cũng xuất hiện 2 lần nhưng mình lấy số lớn hơn.
+# Gợi ý: Sắp xếp tăng dần và đếm tần số
