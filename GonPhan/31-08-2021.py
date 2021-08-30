882c471c9e2ce86a6d00ae054c3dd04a5935f1fd
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-08-30 21:52:35 +0700

    Create 31-08-2021.py

---
+++ GonPhan/31-08-2021.py
@@ -0,0 +1,6 @@
+# Bài 1:
+# Tìm số xuất hiện nhiều nhất trong dãy a. Nếu có nhiều số xuất hiện cùng số lần, đưa ra số lớn nhất.
+# a = [1, 2, 2, 3, 2, 5]
+# Kết quả là 2 vì: 2 xuất hiện 3 lần, là nhiều nhấtype_check_only
+# a = [1, 2, 3, 2, 3]
+# Kết quả là 3, vì 3 xuất hiện 2 lần, số 2 cũng xuất hiện 2 lần nhưng mình lấy số lớn hơn.
