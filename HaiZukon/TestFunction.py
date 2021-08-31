42b23e888b1eba6784dacb322577f832d8ebe7e7
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-08-31 21:23:30 +0700

    Create TestFunction.py

---
+++ HaiZukon/TestFunction.py
@@ -0,0 +1,12 @@
+# def countOdd(a):
+#     count = 0
+#     for i in a:
+#         count += i % 2
+#     return count
+
+def countOdd(a):
+    return sum(1 for i in a if i % 2 == 1)
+
+a = [1, 2, 3, 4, 5, 7]
+print(countOdd(a))
+    
